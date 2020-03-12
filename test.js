29ff268949e329d997576e50db2f4caadae1e6a3
Lorin <lorin@vinoshipper.com>
2020-03-12 10:30:42 -0700

    little bit more test output

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 var covid = require('./lib')
 
 covid.country(['US', 'italy'], (data) => {
-    console.log(data.map(c => c.chart()));
+    console.log(data.map(c => { 
+        return {
+            country:c.country,
+            chart:c.chart() 
+        }
+    }));
 });
